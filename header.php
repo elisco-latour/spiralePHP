770cd720fde40759fadd5930710e9d5dd6c8b7d0
Eli <eliscolatour@gmail.com>
2017-06-22 14:29:33 +0100

    Add  header.php and footer.php
    Separate the header and the footer of the site into 2 different files so
    we can include them in any page by not repaeating the code

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SPIRALE</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/icon?family=Material+Icons'>
+        <link rel="stylesheet"  href="css/style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400" rel="stylesheet">
+    </head>
+    <body>
+        <header class="spirale-header">
+            <div class="spirale-header__menu cf">
+                <h2 class="spirale-header__title">
+                    <a href="#">
+                        <img class="spirale-header__logo" alt="Logo de SPIRALE" src="images/spirale_log_16_9.jpg" />
+                    </a>
+                </h2>
+                <nav class="spirale-header__main-nav" id="HeaderNav">
+                    <ul>
+                        <li><a class="nav-link" href="#">Accueil</a></li>
+                        <li><a class="nav-link" href="#spirale-presentation">A propos</a></li>
+                        <li><a class="nav-link" href="#spirale-reasons">Services</a></li>
+                        <li><a class="nav-link contact-link" href="#spirale-footer">Contactez - nous</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
