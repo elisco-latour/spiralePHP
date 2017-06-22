4c8da8108212033cf49432f45e59ea1e7b9cdb16
Eli <eliscolatour@gmail.com>
2017-06-22 14:51:21 +0100

    Add a contact form to the contact page

--- contact.php
+++ contact.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +6,43 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        /**
+         *  include the header
+         */
+        require_once 'header.php';
+        ?>
+        <main>
+            <section class="spirale-section">
+                <div class="spirale-contact_section">
+                    <div class="spirale-contact_form-container">
+                        <form class="spirale-contact_form">
+                            <label for="nom_contact">Votre nom</label>
+                            <input type="text" name="nom_contact">
+                            <label for="nom_contact">Votre prénom</label>
+                            <input type="text" name="prenom_contact"/>
+                            <label for="nom_contact">Votre adresse email</label>
+                            <input type="email" name="email_contact">
+                            <label for="nom_contact">Votre message</label>
+                            <textarea name="email-content">
+                                
+                            </textarea>
+                        </form>
+                    </div>
+                    <div class="spirale-contact_information">
+                        <div class="info-box">
+                            <p class="spirale-address"></p>
+                            <p class="spirale-phone"></p>
+                            <p class="spirale-email"></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <?php
+        /**
+         * Include the footer
+         */
+        require_once 'footer.php';
         ?>
     </body>
 </html>
