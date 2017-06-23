65c13bef2ebacdead16814ec8f3cc72c09015a33
Eli <eliscolatour@gmail.com>
2017-06-23 01:21:28 +0100

    Add mail sending possibility to the site

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@
             <section class="spirale-section spirale-contact_form-section">
                 <div class="spirale-contact__wrapper">
 
-                    <div class="spirale-contact_information spirale-col--6">
+                    <div class="spirale-contact_information spirale-col--6 spirale-shadow--2dp">
                         <div class="info-box">
                             <p class="spirale-address">Benin / Cotonou, AGOTIKON nouvelle pharmacie</p>
                             <p class="spirale-phone">+299 xx xx xx xx / +229 xx xx xx xx</p>
@@ -24,15 +24,16 @@
                     </div>
 
                     <div class="spirale-contact_form-container spirale-col--6">
-                        <form class="spirale-contact_form">
-                            <label for="nom_contact">Votre nom</label>
+                        <form action="envoi_mail.php" method="POST"  class="spirale-contact_form">
+                            <label for="nom_contact">Votre nom complet</label>
                             <input type="text" name="nom_contact">
-                            <label for="nom_contact">Votre prénom</label>
+                            <label for="nom_contact">Votre sujet</label>
                             <input type="text" name="prenom_contact"/>
                             <label for="nom_contact">Votre adresse email</label>
                             <input type="email" name="email_contact">
                             <label for="nom_contact">Votre message</label>
-                            <textarea name="email-content"></textarea>
+                            <textarea name="message_contact"></textarea>
+                            <input class="contact-link" type="submit" value="Envoyer"/>
                         </form>
                     </div>
 
---
+++ envoi_mail.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ *  SEND A MAIL TO SPIRALE CONTACT BOX
+ */
+
+$mail_status = FALSE;
+
+// Get the last name of the contact
+$nom_contact = trim(filter_input(INPUT_POST, "nom_contact"));
+
+// Get the first Name of the contact
+$prenom_contact = trim(filter_input(INPUT_POST, "prenom_contact"));
+
+// Get the email Address of the contact
+$email_contact = trim(filter_input(INPUT_POST, "email_contact", FILTER_VALIDATE_EMAIL));
+
+// Get the message of the contact
+$message_contact = trim(filter_input(INPUT_POST, "message_contact"));
+
+if ($nom_contact && $prenom_contact && $email_contact && $message_contact) {
+    /**
+     * Configure the mail parameters
+     */
+    // The email Address of Spirale
+    $spirale_contact_mail = "contact@spiralebenin.com";
+
+    $message = str_replace("\n.", "\n..", $message_contact);
+
+    // Les entets supplementaires du mail
+    $headers = 'From: ' . $email_contact . "\r\n" .
+            'Reply-To: ' . $email_contact . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+    $mail_status = mail($spirale_contact_mail, $prenom_contact, $message,  $headers);
+}
+
+
+if($mail_status = TRUE){
+    echo 'Mail Transmis';
+} else {
+    echo "Echec de l'envoi du mail";
+}
+
+
+
+
+
+//echo("nom : ".$nom_contact."<br/>");
+//echo("prenom : ".$prenom_contact."<br/>");
+//echo("Adresse email : ".$email_contact."<br/>");
+//echo("Message : ".$message_contact."<br/>");
